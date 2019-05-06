b6037677412baf36a148b2788f1aa30abf77e549
Bruno Zorzetto <brunozorzetto@gmail.com>
2019-05-06 20:49:25 -0300

    Adicionado mais testes para que o Leilao respeite mais algumas regras - ordem dos lances e respeitar a ordem dos usuarios devem propor um lance

--- src/leilao/test_leilao.py
+++ src/leilao/test_leilao.py
@@ -2,7 +2,7 @@ from unittest import TestCase
 from src.leilao.dominio import Lance, Usuario, Leilao
 
 
-class TestAvaliador(TestCase):
+class TestLeiao(TestCase):
 
     def setUp(self):
         """
@@ -28,19 +28,14 @@ class TestAvaliador(TestCase):
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
 
-    def test_deve_retornar_o_maior_e_o_menor_valor_de_um_lance_quando_adicionados_em_ordem_decrescente(self):
+    def test_nao_deve_permitir_propor_um_lance_em_ordem_decrescente(self):
 
-        batman = Usuario('Batman')
-        lance_batman = Lance(batman, 2000.0)
+        with self.assertRaises(ValueError):
+            batman = Usuario('Batman')
+            lance_batman = Lance(batman, 2000.0)
 
-        self.leilao.propoe(lance_batman)
-        self.leilao.propoe(self.lance_bruno)
-
-        menor_valor_esperado = 500.0
-        maior_valor_esperado = 2000.0
-
-        self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
-        self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+            self.leilao.propoe(lance_batman)
+            self.leilao.propoe(self.lance_bruno)
 
     def test_deve_retornar_o_mesmo_valor_para_o_maior_e_menor_lance_quando_o_leilao_tiver_um_lance(self):
 
@@ -57,8 +52,8 @@ class TestAvaliador(TestCase):
         spider = Usuario('Spider')
         lance_spider = Lance(spider, 8000.0)
 
-        self.leilao.propoe(lance_batman)
         self.leilao.propoe(self.lance_bruno)
+        self.leilao.propoe(lance_batman)
         self.leilao.propoe(lance_spider)
 
         menor_valor_esperado = 500.0
@@ -66,4 +61,30 @@ class TestAvaliador(TestCase):
 
         self.assertEqual(menor_valor_esperado, self.leilao.menor_lance)
         self.assertEqual(maior_valor_esperado, self.leilao.maior_lance)
+
+    def test_deve_permitir_propor_um_lance_caso_o_leilao_nao_tenha_lances(self):
+
+        self.leilao.propoe(self.lance_bruno)
+
+        quantidade_lances_recebida = len(self.leilao.lances)
+        self.assertEqual(1, quantidade_lances_recebida)
+
+    def test_deve_permitir_propror_um_lance_caso_o_ultimo_usuario_seja_diferente(self):
+
+        spider = Usuario('Spider')
+        lance_spider = Lance(spider, 600.0)
+
+        self.leilao.propoe(self.lance_bruno)
+        self.leilao.propoe(lance_spider)
+
+        quantidade_lances_recebido = len(self.leilao.lances)
+        self.assertEqual(2, quantidade_lances_recebido)
+
+    def test_nao_deve_permitir_propor_lance_caso_o_usuario_seja_o_mesmo(self):
+
+        lance_do_bruno = Lance(self.bruno, 900.0)
+
+        with self.assertRaises(ValueError):
+            self.leilao.propoe(self.lance_bruno)
+            self.leilao.propoe(lance_do_bruno)
 
